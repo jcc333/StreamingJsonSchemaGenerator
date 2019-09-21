3e36d898c2d1dc1510fb99d83c613352af1e04ca
jcc333 <clemer19@gmail.com>
2019-09-21 18:53:06 -0400

    Throw an error at the token level for an illegal property or delimiter of arrays or objects

--- StreamingJsonSchemaGenerator.Lib/Token.fs
+++ StreamingJsonSchemaGenerator.Lib/Token.fs
@@ -23,14 +23,13 @@ module Token =
 
     type TokenSeq = seq<TokenResult>
 
-    let validate(typ: JsonToken, tok: obj): TokenResult =
+    let wrapTokensOut(typ: JsonToken, tok: obj): TokenResult =
         match typ with
         | JsonToken.None -> Error("Reader has not yet read.")
         | JsonToken.StartObject -> Result.Ok(StartObject)
         | JsonToken.StartArray -> Ok(StartArray)
         | JsonToken.PropertyName -> Ok(PropertyName(string tok))
-        | JsonToken.Comment -> Ok(Comment)
-        | JsonToken.Integer -> Ok(Number)
+        | JsonToken.Comment -> Ok(Comment) | JsonToken.Integer -> Ok(Number)
         | JsonToken.Float -> Ok(Number)
         | JsonToken.String -> Ok(String)
         | JsonToken.Boolean -> Ok(Boolean)
@@ -42,11 +41,52 @@ module Token =
         | JsonToken.Bytes -> Ok(Bytes)
         | _ -> Error(String.Format("Json '{}': '{}' out of range or out of scope", typ, tok))
 
+    type StakeStackItem = ArrayScope | ObjectScope
+
+    let validateTokenOrder(ts: TokenResult seq) =
+        let rec loop(stack: StakeStackItem list)(ts: TokenResult seq): TokenResult seq =
+            seq {
+                let h = Seq.tryHead ts
+                let hs = Seq.tail ts
+                match h with
+                | None ->
+                    yield! Seq.empty
+                | Some(Ok(StartObject)) ->
+                    yield h.Value
+                    let arrStack = ObjectScope::stack
+                    yield! loop arrStack hs
+                | Some(Ok(StartArray)) ->
+                    yield h.Value
+                    let objStack = ArrayScope::stack
+                    yield! loop objStack hs
+                | Some(Ok(PropertyName(_))) when List.tryHead stack = Some(ObjectScope) ->
+                    yield h.Value
+                    yield! loop stack hs
+                | Some(Ok(PropertyName(_))) ->
+                    yield Error("property names belong in objects")
+                | Some(Ok(EndObject)) when List.tryHead stack = Some(ObjectScope) -> 
+                    yield h.Value
+                    let nonObjStack = List.tail stack
+                    yield! loop nonObjStack hs
+                | Some(Ok(EndObject)) -> 
+                    yield Error("ends of objects belong in objects")
+                | Some(Ok(EndArray)) when List.tryHead stack = Some(ArrayScope) -> 
+                    yield h.Value
+                    let nonArrStack = List.tail stack
+                    yield! loop nonArrStack hs
+                | Some(Ok(EndArray)) ->
+                    yield Error("ends of arrays belong in arrays")
+                | _ ->
+                    yield h.Value
+                    yield! loop stack hs
+            }
+        loop [] ts
+
     let tokens(reader: Newtonsoft.Json.JsonReader) =
         seq { while reader.Read() do yield (reader.TokenType, reader.Value) }
 
     let tokenize(reader: Newtonsoft.Json.JsonReader) =
-        reader |> tokens |> Seq.map validate
+        reader |> tokens |> Seq.map wrapTokensOut |> validateTokenOrder 
 
     let consume(tokens: TokenSeq)(until: TokenResult -> Boolean)(tokenizer: Token -> Result<'a, string>)(exhausted: Result<'a, string>): Result<'a, string> =
         tokens
